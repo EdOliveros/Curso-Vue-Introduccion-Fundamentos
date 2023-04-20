c78a57fe4d53c4e88eea536d4081f7c0d98061af
ed oliveros <edgarolimoy98@gmail.com>
2023-04-20 18:33:58 -0500

    feat: :tada: Curso terminado!

---
+++ hola.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>lorem ipsum dolor</p>
+    <div>{{ text }}</div>
+</template>
+
+<script>
+    export defualt {
+        data() {
+            text: 'hola vue',
+        }
+    }
+</script>
+
+<style>
+    p {
+        color: red;
+    }
+</style>
